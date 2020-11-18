ab9b626e45634aa14215e988e2f3f5466dbba75c
parvinbarzegar <71722674+parvinbarzegar@users.noreply.github.com>
2020-11-18 15:00:12 +0100

    practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [textHeasder, setTextHeader] = useState("Hello");
+
+  function handleClick() {
+    setTextHeader("Submmited");
+  }
+  const [isMouseOver, setStyle] = useState(false);
+  function changeBackground() {
+    setStyle(true);
+  }
+
+  function changeBack() {
+    setStyle(false);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{textHeasder}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        onClick={handleClick}
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+        onMouseOver={changeBackground}
+        onMouseLeave={changeBack}
+      >
+        Submit
+      </button>
     </div>
   );
 }
